2158982ccc674f6e8ef261e2af24db313253a54d
806gw <shjea4364@naver.com>
2025-02-13 17:20:20 +0900

    initial commit

---
+++ study/studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyApp.swift
+//  study
+//
+//  Created by 성홍제 on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ studyTests/studyTests.swift
@@ -0,0 +1,17 @@
+//
+//  studyTests.swift
+//  studyTests
+//
+//  Created by 성홍제 on 2/13/25.
+//
+
+import Testing
+@testable import study
+
+struct studyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
