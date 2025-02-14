1cad67a75204762e5c9e4448932f83db87c567bc
806gw <shjea4364@naver.com>
2025-02-14 23:41:43 +0900

    feat:: 스위프트 타입 종류

--- study/ContentView.swift
+++ study/ContentView.swift
@@ -1,19 +1,23 @@
-//
-//  ContentView.swift
-//  study
-//
-//  Created by 성홍제 on 2/13/25.
-//
+/*
+ 스위프트는 정적 타이핑 언어로, 타입을 명시해주어야함.
+ Int : 64비트 정수형
+ UInt : 부호가 없는 64비트 정수형
+ Float : 32비트 부동 소수점
+ Double : 64비트 부동 소수점
+ Bool : T / F
+ character : 문자
+ String : 문자열 (series of 문자열)
+ Any : 모든 타입을 지칭하는 키워드
+*/
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            var texts: String = "Hello world !"
+            Text(texts)
         }
         .padding()
     }
